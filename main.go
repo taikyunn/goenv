6c3368d10b051049b1aad3238a48f9fd48230e53
taikyunn <taichi_141@me.com>
2021-11-26 15:23:03 +0900

    commit

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 )
 
 func main() {
+	// .envファイルを読み込んでいる。読み込めなかったらエラー。
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error Loading .env file")
 	}
 
+	// os.Getenv(キー名)で値を取得できる
+	// .envはgitignoreさせることを忘れないように。
 	s3Bucket := os.Getenv("S3_BUCKET")
 	secretKey := os.Getenv("SECRET_KEY")
 
